6d68856e7f30cb5979e9e044e2507047edab5616
Viktor Lucaj <vlucaj@gmail.com>
2020-10-13 19:19:18 -0400

    added roosterjs

--- projects/ngx-roosterjs/src/lib/ngx-roosterjs.component.ts
+++ projects/ngx-roosterjs/src/lib/ngx-roosterjs.component.ts
@@ -1,20 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Editor, EditorOptions, EditorPlugin, ContentEdit, Paste, HyperLink, Undo } from 'roosterjs';
+import * as RoosterJs from 'roosterjs';
 
 @Component({
-  selector: 'lib-ngx-roosterjs',
-  template: `
-    <p>
-      ngx-roosterjs works!
-    </p>
-  `,
-  styles: [
-  ]
+  selector: 'ngx-roosterjs',
+  templateUrl: './ngx-roosterjs.component.html',
+  styleUrls: ['./ngx-roosterjs.component.scss']
 })
 export class NgxRoosterjsComponent implements OnInit {
+  public editor: RoosterJs.Editor;
 
-  constructor() { }
+  constructor(private elRef: ElementRef) { }
 
   ngOnInit(): void {
+    this.createEditor();
   }
 
+  private createEditor() {
+    const editorDiv = this.elRef.nativeElement.querySelector('#editorDiv');
+    const plugins: EditorPlugin[] = [
+      new ContentEdit(),
+      new Paste(true /*useDirectPaste*/),
+      new HyperLink(href => 'Ctrl+click to open link ' + href),
+    ];
+    const undo = new Undo(true /*preserveSnapshot*/);
+    const options: EditorOptions = {
+      plugins,
+      undo,
+    };
+
+    this.editor = new Editor(editorDiv, options);
+  }
+
+  ngOnDestroy(): void {
+    this.editor.dispose();
+  }
 }
